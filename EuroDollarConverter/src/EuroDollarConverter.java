caef207ba94022af8863a456e89ac42463611f6f
Selim Karabulut <selim.karabulut@hotmail.de>
2018-10-15 14:33:49 +0200

    Werte geändert!

--- EuroDollarConverter/src/EuroDollarConverter.java
+++ EuroDollarConverter/src/EuroDollarConverter.java
@@ -5,8 +5,8 @@ public class EuroDollarConverter {
 		// TODO Auto-generated method stub
 
 		double kurs = 1.16;
-		double Euro = 167;
-		double Dollar=193.72;
+		double Euro = 198;
+		double Dollar=229.68;
 		
 		System.out.println("Euro zu Dollar: "+(kurs*Euro));
 		System.out.println("Dollar zu Euro: "+(Dollar/kurs));
