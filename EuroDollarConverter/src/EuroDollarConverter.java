16b77c08d4a3291325f773b6498eab75dac75b1a
D070297 <selim.karabulut@sap.com>
2018-10-15 13:29:59 +0200

    First Commit!

---
+++ EuroDollarConverter/src/EuroDollarConverter.java
@@ -0,0 +1,15 @@
+
+public class EuroDollarConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double kurs = 1.16;
+		double Euro = 167;
+		
+		System.out.println(kurs*Euro);
+		
+		
+	}
+
+}
